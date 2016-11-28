12e3b225e2136015391f7155f2871dc141859284
Rusvelt2000 <rusvelt.rr@gmail.com>
2016-11-28 20:27:44 +0100

    styled the nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,11 +3,33 @@ var {Link, IndexLink} = require('react-router');
 
 var Nav = () => {
   return(
-    <div>
-      <h2>This is a Nav</h2>
-      <IndexLink to='/' activeClassName='active'>Get weather</IndexLink>
-      <Link to='/about' activeClassName='active'>About</Link>
-      <Link to='/explore' activeClassName='active'>Explore</Link>
+    <div className='top-bar'>
+      <div className='top-bar-left'>
+        <ul className='menu'>
+          <li className='menu-text'>
+            Catch The Weather
+          </li>
+          <li>
+            <IndexLink to='/' activeClassName='active'>Get weather</IndexLink>
+          </li>
+          <li>
+            <Link to='/about' activeClassName='active'>About</Link>
+          </li>
+          <li>
+            <Link to='/explore' activeClassName='active'>Explore</Link>
+          </li>
+        </ul>
+      </div>
+      <div className='top-bar-right'>
+        <ul className='menu'>
+          <li>
+            <input type="search" placeholder="Search"/>
+          </li>
+          <li>
+            <button type="button" class="button">Search</button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
