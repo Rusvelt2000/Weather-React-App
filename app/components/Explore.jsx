60deed94bcf5933319f7da4e8250314abc215318
Rusvelt2000 <rusvelt.rr@gmail.com>
2016-11-29 21:52:14 +0100

    Centered the main component

--- app/components/Explore.jsx
+++ app/components/Explore.jsx
@@ -1,10 +1,25 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 
 var Explore = (props) => {
   return (
     <div>
-      <h3>Explore</h3>
+      <h1>Explore</h1>
+      <p>Here you can explore anything you'd ever wish to explore in your whole life, get your luggages on and enjoy the trip!</p>
+      <ol>
+        <li className='text-left'>
+          <Link to='/?location=Viterbo'>Viterbo, Italy</Link>
+        </li>
+        <li className='text-left'>
+          <Link to='/?location=Brisbane'>Brisbane, QLD</Link>
+        </li>
+        <li className='text-left'>
+          <Link to='/?location=Sydney'>Sydney, NSW</Link>
+        </li>
+        <li className='text-left'>
+          <Link to='/?location=Perth'>Perth, WA</Link>
+        </li>
+      </ol>
     </div>
   );
 }
